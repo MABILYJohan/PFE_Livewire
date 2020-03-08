1b8fc299de6b329548ad344b6f8b4ad38d9ef48e
MABILYJohan <Johan.MABILY@etu.univ-amu.fr>
2020-03-08 16:52:06 +0100

    ajout d'une partie pcl

---
+++ testJojo/Test10/testpcl.cpp
@@ -0,0 +1,11 @@
+#include "testpcl.h"
+
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/point_types.h>
+//#include <pcl/io/vtk_lib_io.h>
+//#include <pcl/visualization/cloud_viewer.h>
+
+testpcl::testpcl()
+{
+
+}
